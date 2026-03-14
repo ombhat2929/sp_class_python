c1b6840a4e2912ce1de9f5cad76af4200daa9ac8
ombhat2929 <ombhat2030@gmail.com>
2026-03-15 00:21:22 +0530

    ninja_power

---
+++ doremon.py
@@ -0,0 +1,17 @@
+import datetime
+
+# Get today's date
+today = datetime.date.today()
+
+# Day of the week (Monday=0, Sunday=6)
+day_number = today.weekday()
+
+# Day name (like Monday, Tuesday, etc.)
+day_name = today.strftime("%A")
+
+print("Today's date:", today)
+print("Day number:", day_number)
+print("Day name:", day_name)
+
+
+this python code to all problem slove adaaaaaaaaaaaaa
